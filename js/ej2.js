071c77241e1a1090cfdee94dd0febde1b79e57de
Jairo <jairobernaldev@gmail.com>
2018-12-06 18:36:52 +0100

    Actualización

--- js/ej2.js
+++ js/ej2.js
@@ -1,19 +1,19 @@
-var Zombi = function(nombre, puntosVida, potencia) {  
-    this.nombre = nombre; 
-    this.puntosVida = puntosVida;  
-    this.maxVida = puntosVida;  
+var Zombi = function(nombre, puntosVida, potencia) {
+    this.nombre = nombre;
+    this.puntosVida = puntosVida;
+    this.maxVida = puntosVida;
     this.potencia = potencia;
 }
 
-Zombi.prototype.atacar = function(objetivo) {  
-    console.log(this.nombre + " ataca a " + objetivo + " y le causa "+ this.potencia + " puntos de daño");
+Zombi.prototype.atacar = function(objetivo) {
+    console.log(this.nombre + " ataca a " + objetivo + " y le causa " + this.potencia + " puntos de daño");
 }
 
-Zombi.prototype.mover = function(tipoTerreno) {  
-    if (tipoTerreno === "agua") {    
-        console.log(this.nombre + "No se puede mover a través de estetipo de terreno");  
-    } else {    
-        console.log(this.nombre + " se mueve a través del " +tipoTerreno);  
+Zombi.prototype.mover = function(tipoTerreno) {
+    if (tipoTerreno === "agua") {
+        console.log(this.nombre + "No se puede mover a través de estetipo de terreno");
+    } else {
+        console.log(this.nombre + " se mueve a través del " + tipoTerreno);
     }
 }
 
@@ -22,15 +22,15 @@ var zombi = new Zombi('Bob', 2, 3);
 var zombi2 = new Zombi('Tod', 2, 3);
 
 //Le añadimos sus metodos
-zombi.atacar = function(objetivo) {  
-    console.log(this.nombre + " ataca a " + objetivo + " 4 veces y le causa "+ (this.potencia*4) + " puntos de daño");
+zombi.ataqueMultiple = function(objetivo) {
+    console.log(this.nombre + " ataca a " + objetivo + " 4 veces y le causa " + (this.potencia * 4) + " puntos de daño");
 }
 
-zombi2.mover = function(tipoTerreno) {  
-    if (tipoTerreno === "agua") {    
-        console.log(this.nombre + " nada a través del "+tipoTerreno);  
-    } else {    
-        console.log(this.nombre + " se mueve a través del " +tipoTerreno);  
+zombi2.mover = function(tipoTerreno) {
+    if (tipoTerreno === "agua") {
+        console.log(this.nombre + " nada a través del " + tipoTerreno);
+    } else {
+        console.log(this.nombre + " No puede mpverse a través del " + tipoTerreno);
     }
 }
 
