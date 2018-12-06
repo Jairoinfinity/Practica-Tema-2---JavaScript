0506fc92ade33f19e9067e21174ce66f5181319a
Jairo <jairobernaldev@gmail.com>
2018-12-06 19:22:10 +0100

    Actualización

--- js/ej4.js
+++ js/ej4.js
@@ -1,21 +1,21 @@
-var Consumible = function(_nombre, _efecto) {  
-    this.nombre = _nombre;  
+var Consumible = function(_nombre, _efecto) {
+    this.nombre = _nombre;
     this.efecto = _efecto;
 }
 
-Consumible.prototype.consumir = function(personaje) {  
-    console.log(personaje.nombre + " consume " + this.nombre + " y " +this.efecto);
+Consumible.prototype.consumir = function(personaje) {
+    console.log(personaje.nombre + " consume " + this.nombre + " y " + this.efecto);
 }
 
 var manzana = new Consumible('Manzana', 'no tiene ningún efecto');
 
-var ricardo = {  
-    nombre: 'Ricardo',  
-    potencia: 1,  
-    puntosVida: 8,  
-    maxPuntosVida: 10,  
-    consumir: function(consumible) {    
-        consumible.consumir(this);  
+var ricardo = {
+    nombre: 'Ricardo',
+    potencia: 1,
+    puntosVida: 8,
+    maxPuntosVida: 10,
+    consumir: function(consumible) {
+        consumible.consumir(this);
     }
 }
 
@@ -23,47 +23,47 @@ var ricardo = {
 ricardo.consumir(manzana);
 
 //Constructor Planta curativa
-var PlantaCurativa = function(nombre,pVida){
-    Consumible.apply(this,[nombre,"cura su vida."]);
-    this.consumir = function(personaje){
-        
-        var vida = personaje.puntosVida;
-        var maxVida = personaje.maxPuntosVida;
-        var result = maxVida-vida;
-        if(result <= 0){
-            console.log("No puedes consumir esta planta porque tu vida esta completa.");
-        }else{
-            if((pVida + vida) >= maxVida){
-                personaje.puntosVida = maxVida;
-            }else{
-                personaje.puntosVida += pVida;
+var PlantaCurativa = function(nombre, pVida) {
+        Consumible.apply(this, [nombre, "cura su vida."]);
+        this.consumir = function(personaje) {
+
+            var vida = personaje.puntosVida;
+            var maxVida = personaje.maxPuntosVida;
+            var result = maxVida - vida;
+            if (result <= 0) {
+                console.log("No puedes consumir esta planta porque tu vida esta completa.");
+            } else {
+                if ((pVida + vida) >= maxVida) {
+                    personaje.puntosVida = maxVida;
+                } else {
+                    personaje.puntosVida += pVida;
+                }
+                console.log(personaje.nombre + " consume " + this.nombre + " y " + this.efecto);
             }
-            console.log(personaje.nombre + " consume " + this.nombre + " y " +this.efecto);
         }
     }
-}
- //Creamos una planta curativa
-var lechuga = new PlantaCurativa("Lechuga",1);
+    //Creamos una planta curativa
+var lechuga = new PlantaCurativa("Lechuga", 1);
 
 //Constructor Pastilla
-var Pastilla = function(nombre){
-    Consumible.apply(this,[nombre,"aumenta su potencia pero reduce su vida maxima."]);
-    this.consumir = function(personaje){
-        
-        
+var Pastilla = function(nombre) {
+    Consumible.apply(this, [nombre, "aumenta su potencia pero reduce su vida maxima."]);
+    this.consumir = function(personaje) {
+
+
 
         var vida = personaje.puntosVida;
         var maxVida = personaje.maxPuntosVida;
 
-        if(maxVida > 1){
+        if (maxVida > 1) {
             vida = ++personaje.potencia;
             maxVida = --personaje.maxPuntosVida;
 
-            if(vida > maxVida){
+            if (vida > maxVida) {
                 personaje.puntosVida = maxVida;
             }
-            console.log(personaje.nombre + " consume " + this.nombre + " y " +this.efecto);
-        }else{
+            console.log(personaje.nombre + " consume " + this.nombre + " y " + this.efecto);
+        } else {
             console.log("No puedes consumir la pastilla porque ya estas a tope de potencia.");
         }
     }
